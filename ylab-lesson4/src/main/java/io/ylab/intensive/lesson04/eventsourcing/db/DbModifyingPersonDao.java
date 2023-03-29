d0f5ce9c77fbdb0537c1ccc9e7717fdc64338435
Denis Popkov <pkvdenis@gmail.com>
2023-03-29 11:35:33 +0300

    homework-4: добавил методы setNullable в DbModifyingPersonDao.

--- ylab-lesson4/src/main/java/io/ylab/intensive/lesson04/eventsourcing/db/DbModifyingPersonDao.java
+++ ylab-lesson4/src/main/java/io/ylab/intensive/lesson04/eventsourcing/db/DbModifyingPersonDao.java
@@ -6,10 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DbModifyingPersonDao implements ModifyingPersonDao {
     private static final Logger log = LoggerFactory.getLogger(DbModifyingPersonDao.class);
@@ -29,7 +26,7 @@ public class DbModifyingPersonDao implements ModifyingPersonDao {
     public void deletePerson(Long personId) throws SQLException {
         try (Connection connection = this.dataSource.getConnection();
              PreparedStatement deleteStatement = connection.prepareStatement(DELETE_SQL)) {
-            deleteStatement.setLong(1, personId);
+            setNullableLongId(deleteStatement, 1, personId);
             int count = deleteStatement.executeUpdate();
             if (count == 1) {
                 log.info("Person удален по id {}", personId);
@@ -44,7 +41,7 @@ public class DbModifyingPersonDao implements ModifyingPersonDao {
         try (Connection connection = this.dataSource.getConnection()) {
             boolean personExists = false;
             try (PreparedStatement selectStatement = connection.prepareStatement(SELECT_SQL)) {
-                selectStatement.setLong(1, person.getId());
+                setNullableLongId(selectStatement, 1, person.getId());
                 ResultSet resultSet = selectStatement.executeQuery();
                 if (resultSet.next()) {
                     personExists = true;
@@ -52,23 +49,38 @@ public class DbModifyingPersonDao implements ModifyingPersonDao {
             }
             if (personExists) {
                 try (PreparedStatement updateStatement = connection.prepareStatement(UPDATE_SQL)) {
-                    updateStatement.setString(1, person.getName());
-                    updateStatement.setString(2, person.getLastName());
-                    updateStatement.setString(3, person.getMiddleName());
-                    updateStatement.setLong(4, person.getId());
+                    setNullableString(updateStatement, 1, person.getName());
+                    setNullableString(updateStatement, 2, person.getLastName());
+                    setNullableString(updateStatement, 3, person.getMiddleName());
+                    setNullableLongId(updateStatement, 4, person.getId());
                     int count = updateStatement.executeUpdate();
                     log.info("По id {} обновлен {} Person", person.getId(), count);
                 }
             } else {
                 try (PreparedStatement insertStatement = connection.prepareStatement(INSERT_SQL)) {
-                    insertStatement.setLong(1, person.getId());
-                    insertStatement.setString(2, person.getName());
-                    insertStatement.setString(3, person.getLastName());
-                    insertStatement.setString(4, person.getMiddleName());
+                    setNullableLongId(insertStatement, 1, person.getId());
+                    setNullableString(insertStatement, 2, person.getName());
+                    setNullableString(insertStatement, 3, person.getLastName());
+                    setNullableString(insertStatement, 4, person.getMiddleName());
                     int count = insertStatement.executeUpdate();
                     log.info("Добавлен {} Person", count);
                 }
             }
         }
+    }
+
+    private void setNullableString(PreparedStatement statement, int parameterIndex, String value) throws SQLException {
+        if (value != null) {
+            statement.setString(parameterIndex, value);
+        } else {
+            statement.setNull(parameterIndex, Types.VARCHAR);
+        }
+    }
+
+    private void setNullableLongId(PreparedStatement statement, int parameterIndex, Long id) throws SQLException {
+        if (id == null) {
+            throw new NullPointerException("Значение ID не должно быть null");
+        }
+        statement.setLong(parameterIndex, id);
     }
 }
